9b4c044d22866bbfdd7d514f2c31b8441fcaa997
Mike Foley <mfoley@mwfoley.com>
2021-02-01 12:35:41 -0800

    Initial build

---
+++ lib/v_dep.ex
@@ -0,0 +1,20 @@
+defmodule VDep do
+  @moduledoc """
+  Stupid module I'm using to test versioning.
+  """
+
+  @version "1"
+
+  @doc """
+  Simple method returning a version string.
+
+  ## Examples
+
+      iex> VDep.version()
+      "version string"
+
+  """
+  def version do
+    @version
+  end
+end
---
+++ test/v_dep_test.exs
@@ -0,0 +1,8 @@
+defmodule VDepTest do
+  use ExUnit.Case
+  doctest VDep
+
+  test "greets the world" do
+    assert VDep.hello() == :world
+  end
+end
